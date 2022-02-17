cec4408240214b3cefc95e327087f573c61345ba
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-02-16 21:43:43 -0300

    req 7 e 8 (e 6) - ordenacao havia ido por tabela no commit anterior, neste foi adicionada a funcionalidade de colorir o bg do 'li' clicado

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let button = document.getElementById('criar-tarefa')
 button.addEventListener('click', addTask);
 
+
 function addTask() {
-  let newTask = document.getElementById('texto-tarefa');
-  let taskList = document.getElementById('lista-tarefas');
-  let elementNewTask = document.createElement('li');
-  elementNewTask.innerText = newTask.value;
-  taskList.appendChild(elementNewTask);
-  newTask.value = ''; 
+	let newTask = document.getElementById('texto-tarefa');
+	let taskList = document.getElementById('lista-tarefas');
+	let elementNewTask = document.createElement('li');
+	elementNewTask.innerText = newTask.value;
+	elementNewTask.addEventListener('click', highlightItem);
+	taskList.appendChild(elementNewTask);
+	newTask.value = '';
 }
+
+function highlightItem(event) {
+	let highlighted = document.querySelectorAll('li');
+	for (let i = 0; i < highlighted.length; i +=1) {
+		highlighted[i].classList.remove('selectedItem');
+		event.target.classList.add('selectedItem');
+	}
+}
+// resolucao da funcao 'highlightItem' alcancada gracas as intrucoes
+// e testes ocorridos na Sala de Estudos junto aos colegas
+// Ronan Salvador, Vitor Vieira, Jonathan Jhon e Mariza Paini.
